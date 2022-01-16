2e7e272357d81755cf493f42f7163c68141a18ae
Ayoola Taiwo <tayoola13@yahoo.com>
2022-01-16 11:58:35 +0100

    Images now render

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,30 +1,51 @@
 <template>
-  <div class="grid-images">
-    <div class="one">One</div>
+  <div class="grid-images" v-if="ImageData">
+    <!-- <div class="one"></div>
     <div class="two">Two</div>
     <div class="three">Three</div>
     <div class="four">Four</div>
     <div class="five">Five</div>
-    <div class="six">Six</div>
+    <div class="six">Six</div> -->
+    <div
+      class="application-item"
+      v-for="(image, index) in ImageData"
+      v-bind:key="index"
+      @click="$emit('showModal', image)"
+    >
+      <div>
+        <img v-bind:src="image['thumb']" />
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "Images",
-  data(){
-      return({info: '',})
+  data() {
+    return { ImageData: [] };
   },
-  mounted () {
-    axios
-      .get('https://api.unsplash.com/photos?count=6')
-      .then(response => {
-          console.log(response);
-          this.info = response})
-  }
+  async beforeMount() {
+    await axios
+      .create({
+        headers: {
+          Authorization: `Client-ID estcMmHOEb9W3QUAoPQqjpEP_YhBJI7v7ey8RCtnJhE`,
+        },
+      })
+      .get("https://api.unsplash.com/photos?limit=6")
+      .then((response) => {
+        let data = response.data;
+        data.forEach((element, index) => {
+          index < 6 ?this.ImageData.push({...element.urls, ...element.user }) : null;
+        });
+        console.log(this.ImageData);
+
+        return this.ImageData;
+      });
+  },
 };
 </script>
 
@@ -32,8 +53,8 @@ export default {
 .grid-images {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+	border-radius: 10px;
   gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
   width: 80vw;
   margin: 0 auto;
   position: relative;
