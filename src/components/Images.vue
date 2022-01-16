bb5c15650eed905a4b4a95e782c5342c28ad9955
Ayoola Taiwo <tayoola13@yahoo.com>
2022-01-16 08:26:02 +0100

    LAnding page 30% complete

---
+++ src/components/Images.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid-images">
+    <div class="one">One</div>
+    <div class="two">Two</div>
+    <div class="three">Three</div>
+    <div class="four">Four</div>
+    <div class="five">Five</div>
+    <div class="six">Six</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Images",
+};
+</script>
+
+<style lang="scss" scoped>
+.grid-images {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+  width: 80vw;
+  margin: 0 auto;
+  position: relative;
+  bottom: 40px;
+
+  .one {
+    background: red;
+  }
+}
+</style> scoped>
---
+++ src/pages/Landing.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="landing-banner-container">
+    <div class="searchbox-holder">
+      <div class="fa-search-holder"><i class="fa fa-search"></i></div>
+      <input type="text" placeholder="Search for photo" />
+    </div>
+  </div>
+  <Images />
+</template>
+
+<script>
+import Images from "../components/Images.vue";
+export default {
+  name: "Landing",
+  components: { Images },
+};
+</script>
+
+<style lang="scss">
+.landing {
+  &-banner-container {
+    background: #dde2e9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    .searchbox-holder {
+      display: flex;
+      align-items: center;
+      background: #fff;
+      height: 30px;
+      width: 90vw;
+      padding: 5px 15px;
+      border-radius: 8px;
+
+      .fa-search-holder {
+        margin-right: 10px;
+        color: #dde2e9;
+      }
+      input {
+        width: 100%;
+        border: none;
+        outline: none;
+      }
+      ::-webkit-input-placeholder {
+        /* Edge */
+        color: #253858;
+      }
+
+      :-ms-input-placeholder {
+        /* Internet Explorer 10-11 */
+        color: #253858;
+      }
+
+      ::placeholder {
+        color: #253858;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
